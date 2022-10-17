780921ff9d34cd3247976fe70ac616b1602efdeb
PauloHenriqueAS <paulohalves_00@hotmail.com>
2022-10-17 16:08:24 -0300

    Adicionando projeto react

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { clsx } from 'clsx';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    asChild?: boolean;
+}
+
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
+    const Comp = asChild ? Slot : 'button';
+    return (
+        <Comp className={clsx(
+            'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors focus:ring-2 ring-white hover:bg-cyan-200',
+            className,
+            )}
+            {...props}
+        >
+            {children}
+        </Comp>
+    )
+}
